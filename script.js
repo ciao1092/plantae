b8dad00af9dd028592d77f383be5ec6a6e386ca5
Carlo <100089228+ciao1092@users.noreply.github.com>
2024-02-05 16:22:20 +0100

    add name

--- script.js
+++ script.js
@@ -24,31 +24,90 @@ function generate() {
 		return false;
 	}
 	else {
-		var mywindow = window.open('', 'PRINT', 'height=600,width=1200');
+		var newwindow = window.open();
 
-		mywindow.document.write('<html><head><title>' + nomeComune + '</title>');
-		mywindow.document.write('</head><body>');
-		mywindow.document.write('<h1><i>' + genereESpecie + ' (' + nomeComune + ')</i></h1>');
-		mywindow.document.write('<h2>' + "Caratteristiche" + '</h2>');
-		mywindow.document.write('<ul>');
-		mywindow.document.write("<li>Periodo di fioritura: " + periodoDiFioritura + "</li>");
-		mywindow.document.write("<li>Foglie: ");
+		let htmlstring = "<!DOCTYPE html><html lang=\"it\" id=\"root\">";
+
+		htmlstring += ('<head><title>' + nomeComune + '</title><style>.clearfix::after{content:"";clear:both;display:table;}</style>');
+		htmlstring += ('</head><body>');
+		htmlstring += ('<div class="clearfix">');
+		htmlstring += ('<div style="float:left">Nome: ' + getCookie('nome') + '<br />Cognome: ' + getCookie('cognome') + '</div>');
+		htmlstring += ('<div style="float:right">Data: ' + (new Date().toLocaleDateString()) + '</div>');
+		htmlstring += ('</div>');
+		htmlstring += ('<h1><i>' + genereESpecie + ' (' + nomeComune + ')</i></h1>');
+		htmlstring += ('<h2>' + "Caratteristiche" + '</h2>');
+		htmlstring += ('<ul>');
+		htmlstring += ("<li>Periodo di fioritura: " + periodoDiFioritura + "</li>");
+		htmlstring += ("<li>Foglie: ");
 		foglie.split('\n').forEach(line => {
-			mywindow.document.write(line + '<br />');
+			htmlstring += (line + '<br />');
 		});
-		mywindow.document.write("</li>");
+		htmlstring += ("</li>");
 		if (acidofila)
-			mywindow.document.write("<li>&Egrave acidofila</li>");
-		mywindow.document.write("<li>&Egrave " + eaa + "</li>");
-		mywindow.document.write('</ul>');
-		mywindow.document.write('</body></html>');
+			htmlstring += ("<li>&Egrave acidofila</li>");
+		htmlstring += ("<li>&Egrave " + eaa + "</li>");
+		htmlstring += ('</ul>');
+		htmlstring += ('</body>');
+		htmlstring += '</html>';
 
-		mywindow.document.close(); // necessary for IE >= 10
-		mywindow.focus(); // necessary for IE >= 10*/
+		newwindow.document.write(htmlstring);
+		newwindow.document.close();
 
-		mywindow.print();
-		//mywindow.close();
+		newwindow.focus();
+		newwindow.print();
 
 		return true;
 	}
+}
+
+function register() {
+	var nome = document.getElementById('nome').value;
+	var cognome = document.getElementById('cognome').value;
+
+	if (!(nome && cognome)) {
+		document.getElementById('avviso').style.display = "block";
+		return false;
+	} else {
+		setCookie("nome", nome, 1);
+		setCookie("cognome", cognome, 1);
+		window.location = "index.html";
+		return true;
+	}
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function deleteAllCookies() {
+	const cookies = document.cookie.split(";");
+
+	for (let i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i];
+		const eqPos = cookie.indexOf("=");
+		const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	}
+
+	document.location = "registrazione.html";
 }
